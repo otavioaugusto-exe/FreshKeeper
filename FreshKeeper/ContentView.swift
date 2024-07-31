8d9d832b86011944cf4a3bf37e70676cbfc77100
Otávio Augusto <58156476+otavioaugusto-exe@users.noreply.github.com>
2024-07-31 00:55:19 -0300

    Initial Commit

---
+++ FreshKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreshKeeper
+//
+//  Created by Otávio Augusto on 31/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreshKeeper/FreshKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshKeeperApp.swift
+//  FreshKeeper
+//
+//  Created by Otávio Augusto on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FreshKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
